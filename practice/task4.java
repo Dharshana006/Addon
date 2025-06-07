455133baee35ce219db6b05916afb00ce9a40c3f
Dharshana006 <dharshana07006@gmail.com>
2025-06-07 22:03:03 +0530

    day2 version

---
+++ practice/task4.java
@@ -0,0 +1,38 @@
+package practice;
+
+import java.util.Arrays;
+
+public class task4 {
+	
+	public static void reversearray(int arr[])
+	{
+		int left=0;
+		int right=arr.length-1;
+		
+		System.out.println("before sorting "+Arrays.toString(arr));
+		
+		while(left<right)
+		{
+			int temp=arr[left];
+			arr[left]=arr[right];
+			arr[right]=temp;
+			
+			left++;
+			right--;
+		
+		}
+		System.out.println("after sorting "+Arrays.toString(arr));
+	}
+
+	public static void main(String[] args) {
+		int arr[]= {2,4,5,3,8,9,10};
+		
+		Arrays.sort(arr);
+		
+		reversearray(arr);
+		
+		System.out.println("decending order "+ Arrays.toString(arr));
+
+	}
+
+}
---
+++ practice/task7.java
@@ -0,0 +1,44 @@
+package practice;
+
+
+import java.util.Scanner;
+
+public class task7 {
+	
+public static void palindrom(String word) {
+		
+	char arr[]=word.toCharArray();
+	
+		int start=0;
+		int end=arr.length-1;
+		
+		while(start<end) {
+		if(arr[start]!=(arr[end])) {
+			
+			System.out.println("not a palindrome");
+			return;
+	
+		}
+		
+		start++;
+		end--;
+		
+		}
+		System.out.println("palindrome");
+
+		
+}
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		
+		String word=s.nextLine();
+		
+		
+		task7.palindrom(word);
+		
+		
+
+	}
+
+}
